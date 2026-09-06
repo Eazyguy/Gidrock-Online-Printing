aef795a851ce8f01ef7dbb87066e5c5e3e5955af
Eazyguy <israeljhn0@gmail.com>
2026-09-07 00:01:49 +0100

    mobile-fix-2

--- src/app/Sections/HowItWorks.tsx
+++ src/app/Sections/HowItWorks.tsx
@@ -5,7 +5,8 @@ const HowItWorks = () => {
     <div className='mt-8'>
         <h2 className='text-3xl md:text-4xl text-black font-bold text-center'>How it Works</h2>
         <p className='text-gray-700 font-bold text-center md:text-lg'>From upload to doorstep in four simple steps.</p>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-2 mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-2
+        lg:md:grid-cols-4 gap-2 mt-5">
         {howItWorks.map((item)=>(
             <div key={item.title} className="mt-2 bg-green-200 p-5 border border-gray-500 rounded">
                 {item.icon}
